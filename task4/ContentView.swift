72bc3e0dbd2b669c7bec13c22e5b1f8465884ce6
ひろろ <hrsi.ad@gmail.com>
2021-02-16 11:26:24 +0900

    課題４

--- task4/ContentView.swift
+++ task4/ContentView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var result = "0"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        VStack {
+            Text(result)
+                .padding()
+
+            Button("+") {
+                var num = Int(result) ?? 0
+                num += 1
+
+                result = String(num)
+            }.padding()
+
+            Button("clear") {
+                result = "0"
+            }
+
+            Spacer()
+
+        }
+    }}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
